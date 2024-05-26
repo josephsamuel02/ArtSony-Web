2404bba4be0452745ef66ad7a27ccd4c130c29b3
zucm709 <zuckm709@gmail.com>
2024-05-26 14:35:46 +0100

    users art

---
+++ src/constants/index.ts
@@ -0,0 +1,52 @@
+export const userArt = [
+        {
+            id:"1",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"2",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"3",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"4",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"5",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"6",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+        {
+            id:"7",
+            imageUrl:'./assets/icon/frm1.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria'
+
+        },
+
+]
