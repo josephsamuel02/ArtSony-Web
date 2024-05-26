9fefa49f4ed7e0c7f03a1c98c2fcd90383fd269b
zucm709 <zuckm709@gmail.com>
2024-05-27 00:28:17 +0100

    New commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,131 +1,181 @@
 export const UserArtProps = [
         {
             id:"1",
-            imageUrl:'/assets/frm10.png',
+            imageUrl:'/assets/frm1.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
             id:"2",
-            imageUrl:'/assets/frm2.png',
+            imageUrl:'/assets/frm19.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
             id:"3",
-            imageUrl:'/assets/frm3.png',
+            imageUrl:'/assets/frm18.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
+
 
         },
         {
             id:"4",
-            imageUrl:'/assets/frm4.png',
+            imageUrl:'/assets/frm3.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
             id:"5",
-            imageUrl:'/assets/frm5.png',
+            imageUrl:'/assets/frm4.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
             id:"6",
+            imageUrl:'/assets/frm5.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria',
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
+
+        },
+        {
+            id:"7",
             imageUrl:'/assets/frm6.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
-            imageUrl:'/assets/frm11.png',
+            id:"8",
+            imageUrl:'/assets/frm17.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
-            imageUrl:'/assets/frm12.png',
-            name:'John Doe',
-            location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
-
-        },
-        {
-            id:"7",
-            imageUrl:'/assets/frm7.png',
-            name:'John Doe',
-            location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
-
-        },
-        {
-            id:"7",
+            id:"9",
             imageUrl:'/assets/frm8.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
+            id:"10",
             imageUrl:'/assets/frm9.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
+            id:"11",
             imageUrl:'/assets/frm10.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
+            id:"12",
+            imageUrl:'/assets/frm11.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria',
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
+
+        },
+        {
+            id:"13",
+            imageUrl:'/assets/frm12.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria',
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
+
+        },
+        {
+            id:"14",
             imageUrl:'/assets/frm13.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
         {
-            id:"7",
-            imageUrl:'/assets/frm14.png',
-            name:'John Doe',
-            location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
-
-        },
-        {
-            id:"7",
-            imageUrl:'/assets/frm15.png',
-            name:'John Doe',
-            location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
-
-        },
-        {
-            id:"7",
+            id:"15",
             imageUrl:'/assets/frm16.png',
             name:'John Doe',
             location:'Lagos, Nigeria',
-            category:'Lake Side View-Dream Mountain'
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
 
         },
+        {
+            id:"16",
+            imageUrl:'/assets/frm9.png',
+            name:'John Doe',
+            location:'Lagos, Nigeria',
+            category:'Lake Side View-Dream Mountain',
+            imgUrl:'/assets/favorite.png',
+            picUrl:'assets/visibility.png',
+            photoUrl:'assets/profile.png'
+
+        },
+       
 
 ]
--- src/pages/UserPost/UserPost.tsx
+++ src/pages/UserPost/UserPost.tsx
@@ -6,6 +6,9 @@ interface Item {
   name: string;
   location: string;
   category: string;
+  imgUrl:string;
+  picUrl:string;
+  photoUrl:string;
 }
 
 interface UserPostProps {
@@ -20,9 +23,12 @@ const UserPost: React.FC<UserPostProps> = ({ items }) => {
         {items.map((item) => (
           <div key={item.id} style={{ display: 'grid', gridTemplateRows: 'repeat(4, 1fr)', gap: '10px',  borderRadius: '8px', backgroundColor: 'white', padding: '16px' }}>
             <img src={item.imageUrl} alt={item.category} style={{ gridRow: 'span 4', width: '100%', height: 'auto', borderRadius: '8px', marginBottom: '8px' }} />
-            {/* <div style={{ gridRow: 'span 1' }}>
+            <div style={{ display:'flex',gridRow: 'span 1',gap:'10px' }}>
               <p style={{ fontWeight: 'bold' }}>{item.name}</p>
-            </div> */}
+              <img src={item.imgUrl} alt={item.imgUrl}/>
+              <img src={item.picUrl} alt={item.picUrl}/>
+              <img src={item.photoUrl} alt={item.photoUrl}/>
+            </div>
           </div>
         ))}
       </div>
